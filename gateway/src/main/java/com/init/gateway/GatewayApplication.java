3a833b943b7c58295853562f9931a141daf1320b
sinai nin <sinainin.sn@gmail.com>
2023-05-13 22:30:52 +1000

    CGW-1: intail cloud gateway

---
+++ gateway/src/main/java/com/init/gateway/GatewayApplication.java
@@ -0,0 +1,25 @@
+package com.init.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("product-service",r->r.path("/product")
+						.uri("lb://product-service"))
+				.build();
+	}
+}
